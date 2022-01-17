3bd0de136aad06d32d18ed7e91a2775c722b087a
Kelek <kelekofficial@gmail.com>
2022-01-17 18:59:38 +0300

    todolists and tasks

---
+++ Models/TaskItem.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace BlazorApp.Models {
+    public class TaskItem {
+        public string title {get; set;}
+        public bool isDone {get; set;}
+    }
+}
--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 
-/// <summary>
-/// Summary description for TodoItem
-/// </summary>
-
 namespace BlazorApp.Models {
     public class TodoItem {
-        public string Title;
-        public bool IsDone;
+        public string title;
+        public List<TaskItem> tasks;
     }
 }
 
