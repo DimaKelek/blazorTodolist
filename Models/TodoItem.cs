4dcdad8b4d87537dd6b3127c2ed148d641ff630e
Kelek <kelekofficial@gmail.com>
2022-01-17 12:05:29 +0300

    first commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for TodoItem
+/// </summary>
+
+namespace BlazorApp.Models {
+    public class TodoItem {
+        public string Title;
+        public bool IsDone;
+    }
+}
+
